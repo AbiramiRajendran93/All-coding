5bd32b7ab5b2a7e14b17e2d31dce1013c642491c
TestLeaf <TestLeaf@DESKTOP-U97TI4R>
2018-06-09 13:55:06 +0530

    Week3.Day1

---
+++ src/main/java/utils/WebDriverListener.java
@@ -0,0 +1,152 @@
+package utils;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.openqa.selenium.support.events.WebDriverEventListener;
+
+public class WebDriverListener implements WebDriverEventListener{
+
+	public EventFiringWebDriver driver1;
+	public ChromeDriver driver;
+	public WebDriverListener() {
+		System.setProperty("webdriver.chrome.driver", 
+				"./drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver1 = new EventFiringWebDriver(driver);
+		driver1.register(this);
+	}
+	
+	
+	public void beforeAlertAccept(WebDriver driver) {
+	
+		
+	}
+
+	public void afterAlertAccept(WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void afterAlertDismiss(WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void beforeAlertDismiss(WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void beforeNavigateTo(String url, WebDriver driver) {
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	public void afterNavigateTo(String url, WebDriver driver) {
+		System.out.println("The Browser launched Successfully "+url);		
+	}
+
+	public void beforeNavigateBack(WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void afterNavigateBack(WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void beforeNavigateForward(WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void afterNavigateForward(WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void beforeNavigateRefresh(WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void afterNavigateRefresh(WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void afterFindBy(By by, WebElement element, WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void beforeClickOn(WebElement element, WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void afterClickOn(WebElement element, WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void beforeChangeValueOf(WebElement element, WebDriver driver, CharSequence[] keysToSend) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void afterChangeValueOf(WebElement element, WebDriver driver, CharSequence[] keysToSend) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void beforeScript(String script, WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void afterScript(String script, WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void beforeSwitchToWindow(String windowName, WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void afterSwitchToWindow(String windowName, WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onException(Throwable throwable, WebDriver driver) {
+		driver1.close();
+		
+	}
+
+
+	public <X> void afterGetScreenshotAs(OutputType<X> arg0, X arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public <X> void beforeGetScreenshotAs(OutputType<X> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/week2/day1/Login.java
@@ -0,0 +1,108 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import utils.WebDriverListener;
+
+public class Login extends WebDriverListener{
+
+	@Test
+	public void main() throws InterruptedException {
+		// Launch Chrome
+	/*	System.setProperty("webdriver1.chrome.driver", 
+				"./drivers/chromedriver1.exe");
+		ChromeDriver driver = new ChromeDriver();*/
+		// Load URL
+		driver1.get("http://leaftaps.com/opentaps/");
+		
+		driver1.findElement(By.id("username1")).sendKeys("Demo");
+		// maximize browser
+	//	driver1.manage().window().maximize();
+		
+		
+		//Enter user name
+	/*	driver1.findElementById("username").sendKeys("DemoSalesManager");
+		//Enter password
+		driver1.findElementById("password").sendKeys("crmsfa");
+		// Click Login
+		driver1.findElementByClassName("decorativeSubmit").click();
+		// Click Crmsfa link
+		driver1.findElementByLinkText("CRM/SFA").click();
+		driver1.findElementByLinkText("Create Lead").click();
+		// dropdown
+		WebElement source = driver1.findElementById("createLeadForm_dataSourceId");
+		//source.sendKeys("Conference");
+		Select dd = new Select(source);
+		// Select by VisibleText
+		//dd.selectByVisibleText("Direct Mail");
+		// Select By Value
+		//dd.selectByValue("LEAD_PARTNER");
+		List<WebElement> options = dd.getOptions();
+		int size = options.size();
+		System.out.println(size);
+		dd.selectByIndex(size-2);
+		// Print options
+		for (WebElement eachOpt : options) {
+			System.out.println(eachOpt.getText());
+		}
+		
+		driver1.findElementById("createLeadForm_companyName")
+		.sendKeys("Testleaf", Keys.TAB);
+		Thread.sleep(5000);
+		
+		driver1.findElementById("createLeadForm_firstName")
+		.sendKeys("koushik");
+		
+		
+		// Close
+		//driver1.close();
+		
+	*/	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
